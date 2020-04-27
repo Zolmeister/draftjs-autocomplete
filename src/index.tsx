ff59b10aa9db12cdf1756941c8debf2392e3afe0
Zolmeister <zolikahan@gmail.com>
2020-04-27 01:46:37 -0500

    first

---
+++ src/index.tsx
@@ -0,0 +1,237 @@
+import _ from 'lodash'
+import React, {useState, useCallback, useMemo, useContext, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import {Editor, EditorState, ContentState, Modifier, Entity, CompositeDecorator, getDefaultKeyBinding} from 'draft-js'
+
+import 'draft-js/dist/Draft.css'
+import './index.css'
+
+const AUTOCOMPLETE_REGEX = /(^|\s)(#|@|<>)(\S*)/g
+const AUTOCOMPLETE_TYPES = [
+  {
+    key: 'HASH',
+    prefix: '#',
+    class: 'hash'
+  },
+  {
+    key: 'PERSON',
+    prefix: '@',
+    class: 'person'
+  },
+  {
+    key: 'REF',
+    prefix: '<>',
+    class: 'ref'
+  }
+]
+const AUTOCOMPLETE_KEY_TO_TYPE = _.keyBy(AUTOCOMPLETE_TYPES, 'key')
+const AUTOCOMPLETE_PREFIX_TO_TYPE = _.keyBy(AUTOCOMPLETE_TYPES, 'prefix')
+
+const AutocompleteContext = React.createContext({})
+const genResults = {
+  HASH: _.memoize((text) => [...Array(4).keys()].map((_, i) => {
+    return {text: '#' + text + 'h' + Math.random().toString(36).substring(2, i + 4)}
+  })),
+  PERSON: _.memoize((text) => [...Array(4).keys()].map((_, i) => {
+    return {
+      img: `https://placekitten.com/5${i}/5${i}`,
+      text: ' ' + text + Math.random().toString(36).substring(2, i + 4)
+    }
+  })),
+  REF: _.memoize((text) => [...Array(4).keys()].map((_, i) => {
+    return {text: '<>' + text + 'r' + Math.random().toString(36).substring(2, i + 4)}
+  }))
+}
+
+function autocompleteStrategy(contentBlock, callback, contentState) {
+  findWithRegex(AUTOCOMPLETE_REGEX, contentBlock, callback)
+}
+
+// https://github.com/facebook/draft-js/blob/master/examples/draft-0-10-0/tweet/tweet.html
+function findWithRegex(regex, contentBlock, callback) {
+  const text = contentBlock.getText()
+  let matchArr, start
+  while ((matchArr = regex.exec(text)) !== null) {
+    start = matchArr.index
+    callback(start, start + matchArr[0].length)
+  }
+}
+
+const Autocomplete = (props) => {
+  const autocomplete = useContext(AutocompleteContext)
+  const shouldShow = autocomplete &&
+    autocomplete.blockKey === props.blockKey &&
+    autocomplete.start >= props.start &&
+    autocomplete.start <= props.end
+
+  const resultItems = shouldShow ? autocomplete.results.map((result, i) =>
+    <div
+      className={`result ${i === autocomplete.selectedIndex ? 'is-selected' : ''}`}
+      key={i}
+      onMouseOver={() => autocomplete.setSelectedIndex(i)}
+      onClick={() => autocomplete.commitResult(autocomplete.type.key, result, autocomplete.start, autocomplete.end)}>
+      {result.img ? <img src={result.img} width="50" height="50" /> : null}
+      <strong>{result.text.slice(0, autocomplete.matched.length)}</strong>
+      {result.text.slice(autocomplete.matched.length + 1)}
+    </div>
+  ) : []
+
+  return <span className="autocompletable" data-offset-key={props.offsetKey}>
+    {resultItems.length > 0 ? <div className="autocomplete">
+      {resultItems}
+    </div> : null}
+    {props.children}
+  </span>
+}
+
+const autocompleteEntityDecorator = (type) => {
+  return {
+    strategy: (contentBlock, callback, contentState) =>
+      contentBlock.findEntityRanges((x) =>
+        x.getEntity() && contentState.getEntity(x.getEntity()).getType() === type.key
+      , callback),
+    component: (props) => {
+      const result = props.contentState.getEntity(props.entityKey).getData()
+
+      return <span className={`entity ${type.class}`} data-offset-key={props.offsetKey}>
+        {result.img ? <img src={result.img} width="50" height="50" /> : null}
+        {props.children}
+      </span>
+    }
+  }
+}
+
+const compositeDecorator = new CompositeDecorator(
+  AUTOCOMPLETE_TYPES.map(autocompleteEntityDecorator)
+  .concat([{
+    strategy: autocompleteStrategy,
+    component: Autocomplete
+  }])
+)
+
+function MyEditor() {
+  const [editorState, setEditorState] = useState(
+    EditorState.createWithContent(ContentState.createFromText('by Zolmeister #abc @def <>xyz'), compositeDecorator)
+  )
+
+  const commitResult = useCallback((entityType, result, start, end) => {
+    let contentState = editorState.getCurrentContent()
+    const selection = editorState.getSelection().merge({
+      anchorOffset: start,
+      focusOffset: end,
+      isBackward: false
+    })
+
+    contentState = contentState.createEntity(
+      entityType,
+      'IMMUTABLE',
+      result
+    )
+
+    contentState = Modifier.replaceText(
+      contentState,
+      selection,
+      result.text,
+      null,
+      contentState.getLastCreatedEntityKey()
+    )
+
+    contentState = Modifier.insertText(
+      contentState,
+      contentState.getSelectionAfter(),
+      ' '
+    )
+
+    setEditorState(EditorState.push(editorState, contentState))
+  }, [editorState, setEditorState, autocompleteState])
+
+  const [autocompleteIndex, setAutocompleteIndex] = useState(0)
+  const autocompleteState = useMemo(() => {
+    const selection = editorState.getSelection()
+    const anchorKey = selection.getAnchorKey()
+    const anchorOffset = selection.getAnchorOffset()
+    const block = editorState.getCurrentContent().getBlockForKey(anchorKey)
+    const blockText = block.getText()
+    const blockCharacterMetadata = block.getCharacterList()
+
+    let i = anchorOffset
+    while (--i > 0) {
+      if (Boolean(blockCharacterMetadata.get(i).getEntity())) return null
+      if (/\s/.test(blockText[i])) {
+        i += 1
+        break
+      }
+    }
+
+    const match = [...blockText.slice(i, anchorOffset).matchAll(AUTOCOMPLETE_REGEX)]
+    if (match.length === 0) return null
+    const type = AUTOCOMPLETE_PREFIX_TO_TYPE[match[0][2]]
+    const text = match[0][3]
+
+    return {
+      start: i,
+      end: anchorOffset,
+      type: type,
+      text: text,
+      matched: match[0][0],
+      results: genResults[type.key](text),
+      selectedIndex: autocompleteIndex,
+      setSelectedIndex: setAutocompleteIndex,
+      blockKey: anchorKey,
+      commitResult: commitResult
+    }
+  }, [editorState, autocompleteIndex, setAutocompleteIndex, commitResult])
+
+  useEffect(() => {
+    setAutocompleteIndex(0)
+  }, [autocompleteState && autocompleteState.results, setAutocompleteIndex])
+
+  const keyBindingsFn = useCallback((e: SyntheticKeyboardEvent): string => {
+    const isCompleting = Boolean(autocompleteState)
+    if (isCompleting && e.keyCode === 38) { // UP
+      return 'autocomplete-prev'
+    }
+
+    if (isCompleting && e.keyCode === 40) { // DOWN
+      return 'autocomplete-next'
+    }
+
+    if (isCompleting && (e.keyCode === 9 || e.keyCode === 13)) { // TAB, ENTER
+      return 'autocomplete-commit'
+    }
+
+    return getDefaultKeyBinding(e)
+  }, [autocompleteState])
+
+  const handleKeyCommand = useCallback((command: string) => {
+    if (command === 'autocomplete-next') {
+      setAutocompleteIndex((autocompleteIndex + 1) % autocompleteState.results.length)
+      return 'handled'
+    }
+
+    if (command === 'autocomplete-prev') {
+      setAutocompleteIndex(autocompleteIndex === 0 ? autocompleteState.results.length - 1 : autocompleteIndex - 1)
+      return 'handled'
+    }
+
+    if (command === 'autocomplete-commit') {
+      const {type, results, start, end} = autocompleteState
+      autocompleteState.commitResult(type.key, results[autocompleteIndex], start, end)
+      return 'handled'
+    }
+
+    return 'not-handled'
+  }, [autocompleteIndex, setAutocompleteIndex, autocompleteState])
+
+  return <div>
+    <AutocompleteContext.Provider value={autocompleteState}>
+      <Editor
+        editorState={editorState}
+        onChange={setEditorState}
+        keyBindingFn={keyBindingsFn}
+        handleKeyCommand={handleKeyCommand} />
+    </AutocompleteContext.Provider>
+  </div>
+}
+
+ReactDOM.render(<MyEditor />, document.getElementById('container'))
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.tsx'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+   contentBase: './',
+   hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname,
+    publicPath: '/'
+  },
+};
